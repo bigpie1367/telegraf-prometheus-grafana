bc5fb94f7684bb02df2a6ffeb9c3f55b1b58e4f5
bigpie1367 <bigpie1367@gmail.com>
2024-10-29 08:23:33 +0900

    Ref: 컴포즈 파일 경로 수정

--- restart_monitor.sh
+++ restart_monitor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker-compose -p monitor -f monitor.yml down
+docker-compose -p monitor -f ./compose/monitor/monitor.yml down
 
 if [ "$1" == "build" ]; then
-    docker compose -p monitor -f monitor.yml up --build -d
+    docker compose -p monitor -f ./compose/monitor/monitor.yml up --build -d
 else
-    docker compose -p monitor -f monitor.yml up -d
+    docker compose -p monitor -f ./compose/monitor/monitor.yml up -d
 fi
